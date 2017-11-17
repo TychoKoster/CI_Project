e938829af95d8b4d93adba52f5fbf612cd9e07dc
jterstall <jeroen_t@hotmail.com>
2017-11-17 12:46:49 +0100

    Separate output/input data

--- RNN.py
+++ RNN.py
@@ -3,8 +3,8 @@ import read_data as rd
 
 def main():
 	path = '/home/jterstall/Documents/CI/CI_Project/train_data/'
-	category_index, data = rd.read_data(path)
-
+	category_index_input, category_index_output, input_data, output_data = rd.read_data(path)
+	
 
 if __name__ == '__main__':
 	main()
--- read_data.py
+++ read_data.py
@@ -4,8 +4,10 @@ import os
 
 
 def read_data(path):
-	data = []
-	category_index = defaultdict(str)
+	input_data = []
+	output_data = []
+	category_index_input = defaultdict(str)
+	category_index_output = defaultdict(str)
 	for file in os.listdir(path):
 		if file.endswith('.csv'):
 			file_path = path + file
@@ -14,8 +16,13 @@ def read_data(path):
 				# Skip category names
 				row1 = next(reader)
 				for row in reader:
-					data.append(row)
+					output_data.append(row[0:3])
+					input_data.append(row[3:])
 	# Set category indices
-	for i, category in enumerate(row1):
-		category_index[category] = i
-	return category_index, data
+	input_categories = row1[0:3]
+	output_categories = row1[3:]
+	for i, category in enumerate(input_categories):
+		category_index_input[category] = i
+	for i,category in enumerate(output_categories):
+		category_index_output[category] = i
+	return category_index_input, category_index_output, input_data, output_data
